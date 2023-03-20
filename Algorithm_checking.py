48f5cc4e31a36e3f02d63d3a66d6d9ce8f321e9e
Michał Borowski <115993838+Astaranis@users.noreply.github.com>
2023-03-20 21:13:55 +0100

    Add files via upload

---
+++ Algorithm_checking.py
@@ -0,0 +1,22 @@
+"""3. Narysuj schemat blokowy algorytmu, który sprawdza czy podana przez użytkownika wartość
+występuje w tablicy jednowymiarowej. Autor Michał Borowski"""
+
+
+# Dodajemy narzędzia wymagane do przeprowadzenia zadania.
+import numpy as np
+Wartość_użytkownika = int(input(" Wpisz szukaną wartość "))
+
+#####################################################################
+# Tworzymy Tablice
+Jakaś_tam_tablica = np.array([1,2,3,4,5,6,7,8,9,10])
+
+######################################################################
+
+# Tworzymy Algorytm sprawdzający
+for algorytmy_zadanie_3 in Jakaś_tam_tablica:
+    if algorytmy_zadanie_3 == Wartość_użytkownika:
+        print("Ta wartość znajduje się w Tablicy")
+else:
+    print("Tej wartości nie ma w tablicy")
+
+#######################################################################

c9b9b6f091f40c634302c9720050586f437edc5a
Michał Borowski <115993838+Astaranis@users.noreply.github.com>
2023-03-20 21:26:18 +0100

    Add files via upload

---
+++ Minimum_checking.py
@@ -0,0 +1,9 @@
+"""Zaprojektuj algorytm wyszukiwania w tablicy jednowymiarowej minimalnej wartości. """
+
+# Dodajemy potrzebne narzędzia
+import numpy as np
+Tablica =np.array([1,2,3,4,5,6,7,8,9,10,-10])
+###########################################
+# Tworzymy algorytm wyszukujący ( i uswiadamiamy sobie jaki Python jest świetny )
+print(min(Tablica))
+##########################
